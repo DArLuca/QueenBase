c85b9d566642c6d9480c5d038b2da43c147730a9
DArluca <luca.darcangelo@bluewin.ch>
2026-06-28 16:56:24 +0200

    Initial commit: Setup QueenBase Spring Boot project with latest versions

---
+++ QueenBase-Backend/src/main/java/darluca/QueenBase/QueenBaseApplication.java
@@ -0,0 +1,13 @@
+package darluca.QueenBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueenBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueenBaseApplication.class, args);
+	}
+
+}
---
+++ QueenBase-Backend/src/test/java/darluca/QueenBase/QueenBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package darluca.QueenBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueenBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
